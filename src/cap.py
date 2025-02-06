45e593f994fc60ce51ba2a3bb6b9fcc89facefcb
dbsdurjs <whaql12300@gmail.com>
2025-02-06 18:13:33 +0900

    backup 2

--- src/cap.py
+++ src/cap.py
@@ -4,8 +4,8 @@ from transformers import AutoProcessor, AutoModelForImageTextToText
 
 if __name__ == '__main__':
     # folder = "/home/yeogeon/YG_main/diffusion_model/VAD_dataset/UCF-Crimes/UCF_Crimes/Extracted_Frames/Arrest/Arrest001_x264"
-    # base_folder = "./VAD_dataset/UCF-Crimes/UCF_Crimes/Extracted_Frames/"
-    base_folder= "/home/vcl/Documents/YG/VAD_dataset/UCF-Crimes/UCF_Crimes/Extracted_Frames"
+    # base_folder = "/home/yeogeon/YG_main/diffusion_model/VAD_dataset/UCF-Crimes/UCF_Crimes/Extracted_Frames/"   # desktop 경로
+    base_folder= "/media/vcl/DATA/YG/Extracted_Frames"  # 서버 경로
     
     processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-6.7b")
     model = AutoModelForImageTextToText.from_pretrained("Salesforce/blip2-opt-6.7b", torch_dtype=torch.float16)
@@ -14,11 +14,16 @@ if __name__ == '__main__':
     model.to(device)
 
     for class_name in os.listdir(base_folder):
+        classes_names = os.listdir(base_folder)
+        classes_names = classes_names[:8]   # 8개 작업, 서버에서 바꾸기
+        print(f'작업 폴더 이름 : {classes_names}')
+        if class_name not in classes_names:
+            continue
         class_path = os.path.join(base_folder, class_name)
         
         if not os.path.isdir(class_path):  # 디렉토리가 아니면 건너뛰기
             continue
-
+        
         # 4️⃣ 각 동영상 폴더 순회
         for video_folder in os.listdir(class_path):
             video_folder_path = os.path.join(class_path, video_folder)
