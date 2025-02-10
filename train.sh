6c42636835b5b7cec77db6974964ee7794735778
dbsdurjs <whaql12300@gmail.com>
2025-02-10 20:17:57 +0900

    dd

--- train.sh
+++ train.sh
@@ -1,3 +1,2 @@
-# python src/ucf_train.py --model-path='../vadclip_pth/model/model_ucf_10videos.pth' --checkpoint-path='../vadclip_pth/model/checkpoint_10videos.pth'
 python src/ucf_train.py --model-path='../vadclip_pth/model/model_ucf_caption_10videos.pth' --checkpoint-path='../vadclip_pth/model/checkpoint_caption_10videos.pth' --using-caption
 
