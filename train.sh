3fa8007c1a7f53e6357fd5aa9af2e8a55aaaa6d1
dbsdurjs <whaql12300@gmail.com>
2025-02-14 13:40:19 +0900

    sdf

--- train.sh
+++ train.sh
@@ -1,3 +1,7 @@
+# idea4(all_caption) branch
+# caption 사용 : clip feat = clip feat + alpha*cap feat
+# alpha 값을 조절하면서 clip feat와 더하는 방식
+
 # python src/ucf_train.py --model-path='../vadclip_pth/model/model_ucf.pth' --checkpoint-path='../vadclip_pth/model/checkpoint.pth'
 # python src/ucf_test.py --model-path '../vadclip_pth/model/model_ucf.pth' --checkpoint-path '../vadclip_pth/model/checkpoint.pth'
 
