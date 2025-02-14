1b83140be642143c515992a0777b060ef4dd4d4f
dbsdurjs <whaql12300@gmail.com>
2025-02-14 17:46:21 +0900

    as

--- train.sh
+++ train.sh
@@ -8,5 +8,6 @@
 # python src/ucf_train.py --model-path='../vadclip_pth/model/model_ucf_caption(alpha=1).pth' --checkpoint-path='../vadclip_pth/model/checkpoint_caption(alpha=1).pth' --using-caption
 # python src/ucf_test.py --model-path '../vadclip_pth/model/model_ucf_caption(alpha=1).pth' --checkpoint-path '../vadclip_pth/model/checkpoint_caption(alpha=1).pth' --using-caption
 
+# python src/ucf_test.py --model-path '../vadclip_pth/model_ucf_pretrained.pth'
 
 
