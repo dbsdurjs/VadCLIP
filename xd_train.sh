ba5ea0875287e93460d45871df5ca8606436849a
dbsdurjs <whaql12300@gmail.com>
2025-05-08 11:21:27 +0900

    fa

---
+++ xd_train.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python src/xd_train.py --model-path='../vadclip_pth/model/model_xd_caption(server_exp9).pth' --checkpoint-path='../vadclip_pth/model/checkpoint_caption(server_exp9).pth' --using-caption
+CUDA_VISIBLE_DEVICES=0 python src/xd_test.py --model-path '../vadclip_pth/model/model_xd_caption(server_exp9).pth' --checkpoint-path '../vadclip_pth/model/checkpoint_caption(server_exp9).pth' --using-caption --save-test-result
+
+# idea66(29) crossvit적용
