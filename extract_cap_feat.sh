bf35099530a3afc8cbfab7467f3d141d205a601f
dbsdurjs <whaql12300@gmail.com>
2025-05-15 11:28:51 +0900

    af

--- extract_cap_feat.sh
+++ extract_cap_feat.sh
@@ -1,4 +1,7 @@
-python encode_captions_sbert.py \
-  --caption-folder ../captions_json \
-  --caption-feat-folder ../captions_feat_mpnet \
-  --batch-size 256
+CUDA_VISIBLE_DEVICES=0 python src/extract_cap_feat.py \
+    --caption-folder='../VAD_dataset/UCF-Crimes/Extracted_Captions_janus' \
+    --caption-feat-folder='../VAD_dataset/UCF-Crimes/Caption_Features_janus' &
+
+CUDA_VISIBLE_DEVICES=1 python src/extract_cap_feat.py \
+    --caption-folder='../VAD_dataset/XD-Violence/Extracted_Captions_janus' \
+    --caption-feat-folder='../VAD_dataset/XD-Violence/Caption_Features_janus' &
