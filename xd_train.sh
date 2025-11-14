6e06fda79ca32dc31673d50285047815c72432b4
dbsdurjs <whaql12300@gmail.com>
2025-11-14 23:13:05 +0900

    as

--- xd_train.sh
+++ xd_train.sh
@@ -1,3 +1,3 @@
 
-CUDA_VISIBLE_DEVICES=1 python src/xd_train.py --model-path='../vadclip_pth/model/model_xd_caption(server_exp23).pth' --checkpoint-path='../vadclip_pth/model/checkpoint_xd_caption(server_exp23).pth' --using-caption
-CUDA_VISIBLE_DEVICES=1 python src/xd_test.py --model-path '../vadclip_pth/model/model_xd_caption(server_exp23).pth' --checkpoint-path '../vadclip_pth/model/checkpoint_xd_caption(server_exp23).pth' --save-test-result
+CUDA_VISIBLE_DEVICES=1 python src/xd_train.py --model-path='../vadclip_pth/model/graduate_paper_base_xd.pth' --checkpoint-path='../vadclip_pth/model/graduate_paper_base_xd.pth' --using-caption
+CUDA_VISIBLE_DEVICES=1 python src/xd_test.py --model-path '../vadclip_pth/model/graduate_paper_base_xd.pth' --checkpoint-path '../vadclip_pth/model/graduate_paper_base_xd.pth' --save-test-result
